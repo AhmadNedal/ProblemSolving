574f4e16348a68f04145d306573a76ed384ec88f
Ahmad Nedall <ahmadnedal808@gmail.com>
2025-03-06 22:32:25 +0300

    ABBC or BACB.cpp

--- Codeforces/ABBC or BACB.cpp
+++ Codeforces/ABBC or BACB.cpp
@@ -1,60 +1,93 @@
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
+//link : https://codeforces.com/contest/1873/problem/G
+const ll OO = 1e9 * -1;
 
+ll n,k;
+vector<ll> v;
+vector<vector<ll>> dp;
 
-// LinkProblem : https://codeforces.com/contest/455/problem/A
+ll rec(ll idx, ll helthy) {
+    if (helthy < 0) {
+        return OO;
+    }
+    if (idx == n) {
+        return 0;
+    }
+    ll &res = dp[idx][helthy];
 
-
-
-    ll const N = 1e6+2 ;
-    ll n, dp[N] ,arr[N]={0} ;
-    ll lastIndex =0 ;
-
- ll rec(int idx) {
-        if(idx>=lastIndex+1){
-            return 0 ;
-        }
-
-        ll &res=dp[idx] ;
-
-        if(res!=-1){
-            return res;
-        }
-
-        res=0;
-        ll num=max(rec(idx+2)+(arr[idx]*idx),rec(idx+1));
-        res = max (res,num);
-
-        return res ;
-
-
-
-
+    if (res!= -1) {
+        return dp[idx][helthy];
     }
 
+    if (v[idx] < 0) {
+        res = max(rec(idx + 1, helthy), rec(idx + 1, helthy + v[idx]) + 1);
+    } else {
+        res = rec(idx + 1, helthy + v[idx]) + 1;
+    }
+
+    return res;
+}
+
+
+ bool can(ll hei) {
+      ll sum =0 ;
+      for (int i=0;i<n; ++i ) {
+            if (hei-v[i]>0)sum+=(hei-v[i]);
+      }
+      if ( sum <=k) return true ;
+      return false;
+ }
 
 void solve() {
 
-       cin>>n;
-       memset ( arr , 0 , sizeof arr );
-       memset ( dp , -1 , sizeof dp  );
+      string str; cin>>str;
+      n = str.length() ;
+      ll numA= 0,sum=0 ;
+      bool bol = false ;
+      v.clear();
+
+      for(int i=0;i<n;++i) {
+         if(i>0) {
+            if(str[i]=='B'&&str[i+1]=='B')bol=true;
+         }
+         if ( str [i] == 'B') {
+            v.push_back(numA);
+            sum+=numA;
+            numA=0 ;
+         } else {
+            numA++;
+         }
+      }
+
+      if (numA>0){
+         v.push_back(numA);
+         sum +=numA;
+      }
+      
+      if ( str[0] =='B' || str[n-1]=='B'){
+        bol = true ;
+      }
 
 
-       for(ll num,i=0;i<n;++i){
-            cin>>num;
-            arr[num] ++;
-            lastIndex= max(lastIndex,num) ;
-       }
-        cout <<rec(0) ;
+      if ( bol ) {
+        cout<<sum <<"\n";
+      }else {
+        sort (v.begin() , v.end() ) ;
+        cout << sum-v[0] << "\n";
+      }
 
-    }
+
+
+
+
+}
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
-     solve();
-
+    int t; cin>>t;
+    while (t--)solve();
     return 0;
 }
